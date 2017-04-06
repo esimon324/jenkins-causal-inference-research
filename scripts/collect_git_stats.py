60645d0dee8c44260f1e45485efb041096a2717e
Eric Simon <eric.simon324@gmail.com>
2017-04-06 12:38:18 -0400

    Edit to git stats collector to log erroneous repos. Ran script again but still ran out of queries for the day. Need to go back and correct error repos and collect stats on remaining repos.

--- scripts/collect_git_stats.py
+++ scripts/collect_git_stats.py
@@ -15,23 +15,27 @@ def main():
     infile.close()
     
     outpath = os.path.abspath("..\\data\\git_stats.csv")
+    errpath = os.path.abspath("..\\data\\git_repo_errors.txt")
     with open(outpath,'wb') as csvfile:
         csvwriter = csv.writer(csvfile,dialect=csv.excel)
         csvwriter.writerow(['User','Name','Size (KB)','Number of Contributors'])
         
         user,name,url = repos[0]
         gh_user = gh.get_user(user)
-        for user,name,url in repos:
-            if url != 'None':
-                try: 
-                    if gh_user.name != user:
-                        gh_user = gh.get_user(user)
-                    repo = gh_user.get_repo(name)
-                    csvwriter.writerow([user,name,repo.size,num_contributors(repo.get_contributors())])
-                    print user,name
-                except:
-                    print 'ISSUE WITH:',user,name,url
-    csvfile.close()       
+        with open(errpath,'wb') as errfile:
+            for user,name,url in repos:
+                if url != 'None':
+                    try: 
+                        if gh_user.name != user:
+                            gh_user = gh.get_user(user)
+                        repo = gh_user.get_repo(name)
+                        csvwriter.writerow([user,name,repo.size,num_contributors(repo.get_contributors())])
+                        print user,name
+                    except:
+                        outstr = user+','+name+','+url+'\n'
+                        errfile.write(outstr)
+        errfile.close()
+    csvfile.close()
     
 def num_contributors(contributors):
     num = 0
